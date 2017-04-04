b20021571ce9f2e87089dcb48fccc2aad1754267
Matthew D. Huff <matthewhuff89@gmail.com>
2017-04-04 19:58:58 -0400

    Completed exercise 1.3

--- chapter1/1_3.scm
+++ chapter1/1_3.scm
@@ -1,17 +1,20 @@
-#; Define a procedure that takes three numbers and returns sum of squares of two highest
-(define (sum-of-largest-squares a b c) 
-  (cond ((> a b) (+ (square a) (square b) )
-                 
-  (+ (square a) (square b))
-  )
-
-#; Define a function for getting the square of an integer
 (define (square x) (* x x))
 
-#; Define a function for getting the two highest of three numbers
-(define (high-2 x y z) 
+(define (highest x y z) 
   (cond ((and (> x y) (> x z)) x) 
         ((and (> y x) (> y z)) y)
         ((and (> z x) (> z y)) z)
-        )
-  )
+        ))
+
+(define (second-highest x y z)
+  (cond ((and (> x y) (< x z)) x)
+        ((and (> y x) (< y z)) y)
+        ((and (> z y) (< z x)) z) 
+        ))
+
+(define (sum-of-largest-squares x y z) 
+        (+ (square (highest x y z))
+           (square (second-highest x y z))))
+
+
+(write (sum-of-largest-squares 1 2 3))
